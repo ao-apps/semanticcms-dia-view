c652932f992f45fc5dd9b8110faefe3106cd7b69
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:51:07 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-view - SemanticCMS view of all Dia-based diagrams in the current page and all children.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,13 +21,13 @@
  * along with semanticcms-dia-view.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.dia.view {
-	exports com.semanticcms.dia.view;
-	// Direct
-	requires com.aoapps.html.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-servlet</artifactId>
-	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
-	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
-	requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
-	requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
-	requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
-	requires com.semanticcms.dia.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-model</artifactId>
+  exports com.semanticcms.dia.view;
+  // Direct
+  requires com.aoapps.html.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-servlet</artifactId>
+  requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+  requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+  requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
+  requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
+  requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
+  requires com.semanticcms.dia.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-model</artifactId>
 }
